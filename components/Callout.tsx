570bb0826e1e45cebb2f1292a4780b3117a46ace
demola malomo <malomo.alaba@yahoo.com>
2024-02-17 01:00:51 +0100

    callout fix

--- components/Callout.tsx
+++ components/Callout.tsx
@@ -1,25 +1,29 @@
-import * as React from 'react';
+interface CalloutProps {
+	icon?: string;
+	title?: string;
+	children?: React.ReactNode;
+}
 
-export function Callout({ title, children }) {
-  return (
-    <div className="callout">
-      <strong>{title}</strong>
-      <span>{children}</span>
-      <style jsx>
-        {`
-          .callout {
-            display: flex;
-            flex-direction: column;
-            padding: 12px 16px;
-            background: #f6f9fc;
-            border: 1px solid #dce6e9;
-            border-radius: 4px;
-          }
-          .callout :global(p) {
-            margin: 0;
-          }
-        `}
-      </style>
-    </div>
-  );
+export function Callout({ title, children }: CalloutProps) {
+	return (
+		<div className='callout'>
+			<strong>{title}</strong>
+			<span>{children}</span>
+			<style jsx>
+				{`
+					.callout {
+						display: flex;
+						flex-direction: column;
+						padding: 12px 16px;
+						background: #f6f9fc;
+						border: 1px solid #dce6e9;
+						border-radius: 4px;
+					}
+					.callout :global(p) {
+						margin: 0;
+					}
+				`}
+			</style>
+		</div>
+	);
 }
