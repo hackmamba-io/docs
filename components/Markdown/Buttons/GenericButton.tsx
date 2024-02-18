23bd6f930feebc3cbb830a726e56eb5d1138deb6
Treasure O <treasure@tronweb.co>
2024-02-18 20:29:34 +0100

    feat: added other types of menu and styleing for internal text

---
+++ components/Markdown/Buttons/GenericButton.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function GenericButton() {
+  return <div>GenericButton</div>;
+}
--- components/Markdown/Menus/PopMenu.tsx
+++ components/Markdown/Menus/PopMenu.tsx
@@ -12,19 +12,28 @@ export default function PopMenu(props: { editor: Editor | null }) {
       >
         <button
           onClick={() => editor.chain().focus().toggleBold().run()}
-          className={editor.isActive("bold") ? "is-active" : ""}
+          className={
+            (editor.isActive("bold") ? "p-3 bg-slate-700" : "") +
+            "p-3 bg-slate-100"
+          }
         >
           Bold
         </button>
         <button
           onClick={() => editor.chain().focus().toggleItalic().run()}
-          className={editor.isActive("italic") ? "is-active" : ""}
+          className={
+            (editor.isActive("italic") ? "p-3 bg-slate-700" : "") +
+            "p-3 bg-slate-100"
+          }
         >
           Italic
         </button>
         <button
           onClick={() => editor.chain().focus().toggleStrike().run()}
-          className={editor.isActive("strike") ? "is-active" : ""}
+          className={
+            (editor.isActive("strike") ? "p-3 bg-slate-700" : "") +
+            "p-3 bg-slate-100"
+          }
         >
           Strike
         </button>
