de4eb5049b91ed432c07347d94dfb6bd445e9b6a
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2024-03-03 20:19:28 +0100

    update implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,29 +3,35 @@ import { siteMetaConfig } from '@/config/site.config';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import PostHog, { PostHogConfig } from 'posthog-js';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
-const options: Partial<PostHogConfig> = {
-  api_host: process.env.PUBLIC_POSTHOG_HOST || '',
-};
-
-PostHog.init(process.env.PUBLIC_POSTHOG_KEY || '', options);
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug();
+    },
+  });
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Head>
-        <title>{siteMetaConfig.name}</title>
-        <meta name={siteMetaConfig.name} content={siteMetaConfig.name} />
-        <meta
-          name='keywords'
-          content={siteMetaConfig.keywords.join(',')}
-        ></meta>
-        <meta name='description' content={siteMetaConfig.description}></meta>
+    <PostHogProvider client={posthog}>
+      <Layout>
+        <Head>
+          <title>{siteMetaConfig.name}</title>
+          <meta name={siteMetaConfig.name} content={siteMetaConfig.name} />
+          <meta
+            name='keywords'
+            content={siteMetaConfig.keywords.join(',')}
+          ></meta>
+          <meta name='description' content={siteMetaConfig.description}></meta>
 
-        <link rel='icon' href='/assets/siteLogo.svg' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+          <link rel='icon' href='/assets/siteLogo.svg' />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </PostHogProvider>
   );
 }
